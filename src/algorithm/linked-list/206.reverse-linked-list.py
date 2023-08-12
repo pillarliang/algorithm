ddc485ee79e7921403493d26c1a339732467cbb1
pillarliang <pillarliang21@gmail.com>
2023-08-12 22:10:45 +0800

    add l206

---
+++ src/algorithm/linked-list/206.reverse-linked-list.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=206 lang=python3
+#
+# [206] Reverse Linked List
+#
+
+
+# @lc code=start
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+from typing import Optional
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        pre = None
+        while cur:
+            tmp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = tmp
+        return pre
+
+
+# @lc code=end
