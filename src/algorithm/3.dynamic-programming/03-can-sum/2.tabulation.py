d6e0e53a94e5059a420b9126eac262032a7962f4
pillarliang <pillarliang21@gmail.com>
2023-10-09 11:36:25 +0800

    add dp

--- src/algorithm/3.dynamic-programming/03-can-sum/2.tabulation.py
+++ src/algorithm/3.dynamic-programming/03-can-sum/2.tabulation.py
@@ -16,3 +16,19 @@ print(can_sum(7, [5, 3, 4, 7]))  # True
 print(can_sum(7, [2, 4]))  # False
 print(can_sum(8, [2, 3, 5]))  # True
 print(can_sum(300, [7, 14]))  # False
+
+
+class Solution:
+    def can_sum(self, target: int, numbers: list) -> int:
+        table = [False] * (target + 1)
+        table[0] = True
+
+        for index, item in enumerate(table):
+            if item is False:
+                continue
+
+            for num in numbers:
+                if index + num < target:
+                    table[index + num] = True
+
+        return table[target]
