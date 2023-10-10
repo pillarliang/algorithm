0a0081c38e95a91c6c8f6d6a0ac7060a3ac1888d
pillarliang <pillarliang21@gmail.com>
2023-10-10 10:36:00 +0800

    add dp

--- src/algorithm/3.dynamic-programming/04-how-sum/1.memoization.py
+++ src/algorithm/3.dynamic-programming/04-how-sum/1.memoization.py
@@ -15,30 +15,54 @@
 #     return None
 """memoization"""
 
+# def how_sum(target, numbers, memo={}):
+#     if target in memo:
+#         return memo[target]
 
-def how_sum(target, numbers, memo={}):
-    if target in memo:
+#     if target < 0:
+#         return None
+#     if target == 0:
+#         return []
+
+#     for num in numbers:
+#         remainderResult = how_sum(target - num, numbers, memo)
+
+#         if remainderResult is not None:
+#             remainderResult.append(num)
+#             memo[target] = remainderResult
+#             return remainderResult
+
+#     memo[target] = None
+#     return None
+
+# print(how_sum(7, [2, 3]))  # [3, 2, 2]
+# print(how_sum(7, [5, 3, 4, 7]))  # [4, 3]
+# print(how_sum(7, [2, 4]))  # None
+# print(how_sum(8, [2, 3, 5]))  # [2, 2, 2, 2]
+# print(how_sum(300, [7, 14]))  # None
+
+
+class Solution:
+    def how_sum(self, target, numbers, memo={}):
+        if target in memo:
+            return memo[target]
+        if target == 0:
+            return []
+        if target < 0:
+            return None
+
+        for num in numbers:
+            res = self.how_sum(target - num, numbers, memo)
+            if res is not None:
+                res.append(num)
+                memo[target] = res
+                return memo[target]
+        memo[target] = None
         return memo[target]
 
-    if target < 0:
-        return None
-    if target == 0:
-        return []
 
-    for num in numbers:
-        remainderResult = how_sum(target - num, numbers, memo)
-
-        if remainderResult is not None:
-            remainderResult.append(num)
-            memo[target] = remainderResult
-            return remainderResult
-
-    memo[target] = None
-    return None
-
-
-print(how_sum(7, [2, 3]))  # [3, 2, 2]
-print(how_sum(7, [5, 3, 4, 7]))  # [4, 3]
-print(how_sum(7, [2, 4]))  # None
-print(how_sum(8, [2, 3, 5]))  # [2, 2, 2, 2]
-print(how_sum(300, [7, 14]))  # None
+# print(Solution().how_sum(7, [2, 3]))  # [3, 2, 2]
+# print(Solution().how_sum(7, [5, 3, 4, 7]))  # [4, 3]
+# print(Solution().how_sum(7, [2, 4]))  # None
+# print(Solution().how_sum(8, [2, 3, 5]))  # [2, 2, 2, 2]
+print(Solution().how_sum(300, [7, 14]))  # None
--- src/algorithm/3.dynamic-programming/04-how-sum/2.tabulation.py
+++ src/algorithm/3.dynamic-programming/04-how-sum/2.tabulation.py
@@ -1,20 +1,40 @@
-def how_sum(target: int, numbers: list) -> int:
-    table = [None] * (target + 1)
-    table[0] = []
-    for index, item in enumerate(table):
-        if item is None:
-            continue
+# def how_sum(target: int, numbers: list) -> int:
+#     table = [None] * (target + 1)
+#     table[0] = []
+#     for index, item in enumerate(table):
+#         if item is None:
+#             continue
 
-        for num in numbers:
-            if index + num <= target:
-                table[index + num] = item[:]
-                table[index + num].append(num)
+#         for num in numbers:
+#             if index + num <= target:
+#                 table[index + num] = item[:]
+#                 table[index + num].append(num)
 
-    return table[target]
+#     return table[target]
+
+# print(how_sum(7, [2, 3]))  # [3, 2, 2]
+# print(how_sum(7, [5, 3, 4, 7]))  # [4, 3]
+# print(how_sum(7, [2, 4]))  # None
+# print(how_sum(8, [2, 3, 5]))  # [2, 2, 2, 2]
+# print(how_sum(300, [7, 14]))  # None
 
 
-print(how_sum(7, [2, 3]))  # [3, 2, 2]
-print(how_sum(7, [5, 3, 4, 7]))  # [4, 3]
-print(how_sum(7, [2, 4]))  # None
-print(how_sum(8, [2, 3, 5]))  # [2, 2, 2, 2]
-print(how_sum(300, [7, 14]))  # None
+class Solution:
+    def how_sum(self, target, numbers):
+        table = [None] * (target + 1)
+        table[0] = []
+
+        for i in range(target + 1):
+            if table[i] is None:
+                continue
+            for num in numbers:
+                if i + num <= target:
+                    table[i + num] = table[i][:]
+                    table[i + num].append(num)
+
+        return table[target]
+
+
+print(Solution().how_sum(7, [2, 3]))
+print(Solution().how_sum(7, [5, 3, 4, 7]))
+print(Solution().how_sum(7, [2, 4]))
