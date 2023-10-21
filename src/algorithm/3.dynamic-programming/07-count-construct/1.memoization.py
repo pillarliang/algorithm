ab3c94563f2ea74c2d23ae9cde2939ebf747f7f1
pillarliang <pillarliang21@gmail.com>
2023-10-21 10:17:16 +0800

    add can-construct

--- src/algorithm/3.dynamic-programming/07-count-construct/1.memoization.py
+++ src/algorithm/3.dynamic-programming/07-count-construct/1.memoization.py
@@ -12,37 +12,54 @@
 #     return total_count
 """memoization"""
 
+# def count_construct(target: str, word_bank: list, memo={}):
+#     if target in memo:
+#         return memo[target]
 
-def count_construct(target: str, word_bank: list, memo={}):
-    if target in memo:
+#     if target == '':
+#         return 1
+#     total_count = 0
+#     for word in word_bank:
+#         if target.find(word) == 0:
+#             count = count_construct(target[len(word):], word_bank)
+#             total_count += count
+
+#     memo[target] = total_count
+#     return total_count
+
+
+class Solution():
+    def count_construct(self, target: str, word_bank: list, memo={}):
+        if target in memo:
+            return memo[target]
+
+        if target == '':
+            return 1
+
+        total_count = 0
+        for word in word_bank:
+            if target.find(word) == 0:
+                count = self.count_construct(target[len(word):], word_bank,
+                                             memo)
+                total_count += count
+
+        memo[target] = total_count
         return memo[target]
 
-    if target == '':
-        return 1
-    total_count = 0
-    for word in word_bank:
-        if target.find(word) == 0:
-            count = count_construct(target[len(word):], word_bank)
-            total_count += count
 
-    memo[target] = total_count
-    return total_count
-
-
-print(count_construct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))  # 2
-print(count_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))  # 1
-print(
-    count_construct("skateboard",
-                    ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))  # 0
-print(
-    count_construct("enterapotentpot",
-                    ["a", "p", "ent", "enter", "ot", "o", "t"]))  # 4
-print(
-    count_construct("eeeeeeeeeeeeeeeeeeeef",
-                    ['e'
-                     'ee'
-                     'eee'
-                     'eeee'
-                     'eeeee'
-                     'eeeeee']))
+print(Solution().count_construct('purple',
+                                 ['purp', 'p', 'ur', 'le', 'purpl']))  # 2
+print(Solution().count_construct("abcdef",
+                                 ["ab", "abc", "cd", "def", "abcd"]))  # 1
+print(Solution().count_construct(
+    "skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))  # 0
+print(Solution().count_construct(
+    "enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))  # 4
+print(Solution().count_construct("eeeeeeeeeeeeeeeeeeeef",
+                                 ['e'
+                                  'ee'
+                                  'eee'
+                                  'eeee'
+                                  'eeeee'
+                                  'eeeeee']))
 # 0
--- src/algorithm/3.dynamic-programming/07-count-construct/2.tabulation.py
+++ src/algorithm/3.dynamic-programming/07-count-construct/2.tabulation.py
@@ -1,30 +1,43 @@
-def count_construct(target: str, word_bank: list):
-    table = [0] * (len(target) + 1)
-    table[0] = 1
+# def count_construct(target: str, word_bank: list):
+#     table = [0] * (len(target) + 1)
+#     table[0] = 1
 
-    for index, item in enumerate(table):
-        if item != 0:
-            for word in word_bank:
-                if target[index:(index + len(word))] == word:
-                    table[index + len(word)] += item
+#     for index, item in enumerate(table):
+#         if item != 0:
+#             for word in word_bank:
+#                 if target[index:(index + len(word))] == word:
+#                     table[index + len(word)] += item
 
-    return table[len(target)]
+#     return table[len(target)]
 
 
-print(count_construct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))  # 2
-print(count_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))  # 1
-print(
-    count_construct("skateboard",
-                    ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))  # 0
-print(
-    count_construct("enterapotentpot",
-                    ["a", "p", "ent", "enter", "ot", "o", "t"]))  # 4
-print(
-    count_construct("eeeeeeeeeeeeeeeeeeeef",
-                    ['e'
-                     'ee'
-                     'eee'
-                     'eeee'
-                     'eeeee'
-                     'eeeeee']))
+class Solution():
+    def count_construct(self, target: str, word_bank: list):
+        table = [0] * (len(target) + 1)
+        table[0] = 1
+
+        for index, item in enumerate(table):
+            if item != 0:
+                for word in word_bank:
+                    if target[index:(index + len(word))] == word:
+                        table[index + len(word)] += item
+
+        return table[len(target)]
+
+
+print(Solution().count_construct('purple',
+                                 ['purp', 'p', 'ur', 'le', 'purpl']))  # 2
+print(Solution().count_construct("abcdef",
+                                 ["ab", "abc", "cd", "def", "abcd"]))  # 1
+print(Solution().count_construct(
+    "skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))  # 0
+print(Solution().count_construct(
+    "enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))  # 4
+print(Solution().count_construct("eeeeeeeeeeeeeeeeeeeef",
+                                 ['e'
+                                  'ee'
+                                  'eee'
+                                  'eeee'
+                                  'eeeee'
+                                  'eeeeee']))
 # 0
