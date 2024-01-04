8bc24ec8d35c899791c0f908aa92a6f5055411e9
pillarliang <pillarliang21@gmail.com>
2024-01-04 19:40:34 +0800

    adjust code structure

--- src/algorithm/02.linked-list/19.remove-nth-node-from-end-of-list.py
+++ src/algorithm/02.linked-list/19.remove-nth-node-from-end-of-list.py
@@ -7,13 +7,13 @@
 
 # @lc code=start
 # Definition for singly-linked list.
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
-
-from typing import Optional
 
 
 class Solution:
