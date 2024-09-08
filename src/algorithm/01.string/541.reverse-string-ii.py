20bc47a6a2ddd3e4aa3f17513b9716a8cc43b8b1
pillar <pillarliang21@gmail.com>
2024-09-08 11:31:48 +0800

    add some algorithm

--- src/algorithm/01.string/541.reverse-string-ii.py
+++ src/algorithm/01.string/541.reverse-string-ii.py
@@ -7,13 +7,24 @@
 
 # @lc code=start
 class Solution:
-    def reverseStr(self, s: str, k: int) -> str:
+
+    def reverseStr_old(self, s: str, k: int) -> str:
         count = 0
         while count < len(s):
             count2 = count + k
             s = s[:count] + s[count:count2][::-1] + s[count2:]
             count = count2 + k
         return s
+
+    def reverseStr(self, s: str, k: int) -> str:
+        # 将字符串转化为列表，这样我们可以就地修改字符
+        list_s = list(s)
+
+        for i in range(0, len(s), 2 * k):
+            # 反转前 k 个字符
+            list_s[i : i + k] = reversed(list_s[i : i + k])
+
+        return "".join(list_s)
 
 
 # @lc code=end
