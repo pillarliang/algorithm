b9033ebce0d1889a28b334a6343c5f8c5e3c1df1
pillar <pillarliang21@gmail.com>
2024-09-18 10:16:15 +0800

    fix error

---
+++ src/algorithm/08.dynamic-programming/2049.count-nodes-with-the-highest-score.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2049 lang=python3
+#
+# [2049] Count Nodes With the Highest Score
+#
+
+# @lc code=start
+class Solution:
+    def countHighestScoreNodes(self, parents: List[int]) -> int:
+        
+# @lc code=end
+
